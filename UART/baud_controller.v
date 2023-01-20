d57fc811e28113673ba915ab8e7257ff7868e23b
tsamourid <thanasistsam@yahoo.com>
2023-01-20 17:46:37 +0200

    Revert "Fixed baud controller issue"
    This reverts commit 9024eafa2d49a8515cb46b146b00d9328e498757.

--- UART/baud_controller.v
+++ UART/baud_controller.v
@@ -5,21 +5,21 @@ input clk, reset;
 input [2:0] baud_select; 
 output sample_ENABLE;
 
-reg [13:0] counter =0;
+reg [4:0] counter;
 reg sample_ENABLE;
-reg [13:0] reverse_sample_ENABLE = 13'd0;
+reg [4:0] reverse_sample_ENABLE = 5'd00000;
 
 always @ (baud_select)
 begin
     case(baud_select)
-        3'b000 : reverse_sample_ENABLE = 14'd10417;
-        3'b001 : reverse_sample_ENABLE = 14'd2604;
-        3'b010 : reverse_sample_ENABLE = 14'd651;
-        3'b011 : reverse_sample_ENABLE = 14'd326;
-        3'b100 : reverse_sample_ENABLE = 14'd163;
-        3'b101 : reverse_sample_ENABLE = 14'd81;
-        3'b110 : reverse_sample_ENABLE = 14'd54;
-        3'b111 : reverse_sample_ENABLE = 14'd27;
+        3'b000 : reverse_sample_ENABLE = 10417;
+        3'b001 : reverse_sample_ENABLE = 2604;
+        3'b010 : reverse_sample_ENABLE = 651;
+        3'b011 : reverse_sample_ENABLE = 326;
+        3'b100 : reverse_sample_ENABLE = 163;
+        3'b101 : reverse_sample_ENABLE = 81;
+        3'b110 : reverse_sample_ENABLE = 54;
+        3'b111 : reverse_sample_ENABLE = 27;
     endcase  
 end
 
@@ -29,18 +29,18 @@ begin
 	if (reset)
 	begin
         sample_ENABLE = 1'b0;
-		counter = 14'd0;
+		counter = 5'd1;
 	end	
 	else 
 	begin
 		if (counter == reverse_sample_ENABLE)
 		begin
-			counter = 14'd0;
+			counter = 5'd1;
             sample_ENABLE = ~ sample_ENABLE;
 		end
 		else
 		begin
-			counter = counter + 1;
+			counter = counter + 1'd1;
 		end
 	end
 end
