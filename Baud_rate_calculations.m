89b2dd72af5e370488ac7b24294d3f855abc0131
tsamourid <thanasistsam@yahoo.com>
2023-01-20 17:57:55 +0200

    Add clearing of workspace in matlab script

---
+++ Baud_rate_calculations.m
@@ -0,0 +1,20 @@
+clear all; clc; close all
+
+baud_rate = [300 1200 4800 9600 19200 38400 57600 115200];
+
+T_sample_desired_nanosec = 1./(16.*baud_rate) .* 1e9;
+T_half_sample_desired_nanosec = T_sample_desired_nanosec./2;
+
+clocks_till_reverse = round(T_half_sample_desired_nanosec./20)
+
+
+
+
+
+
+
+
+
+
+
+
