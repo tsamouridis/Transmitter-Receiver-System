ed093a1ef02d8b8b6c941138cffdb2f2afb89dc0
tsamourid <thanasistsam@yahoo.com>
2023-01-28 18:16:08 +0200

    Add Error Detection in register

--- UART/register.v
+++ UART/register.v
@@ -2,29 +2,41 @@
 
 // ! add ferror perror as input to give error output for the led
 
-module register(reset, data_in, valid, out);
+module register(reset, data_in, valid, PERROR, FERROR, out);
 
 input[7:0] data_in;
-input reset, valid;
+input reset, valid, PERROR, FERROR;
 output [15:0] out;
 
 reg[15:0] out;
 reg[7:0] temp1;
 reg[2:0] counter;
+reg error_detected;
+
+// output when error occurs
+parameter FFFF = 16'b1100_1100_1100_1100;
   
-// fix temp_storage_index initialization
 always @ (posedge valid) begin 
-      if(counter == 1) 
-         temp1 <= data_in;
-      else begin
-         out[7:0] <= temp1;
-         out[15:8] <= data_in;
-      end
+	if(counter == 1) 
+	temp1 <= data_in;
+	else begin
+		if (error_detected == 1'b0) begin
+			out[7:0] <= temp1;
+			out[15:8] <= data_in;
+		end
+		else begin
+			out <= FFFF;
+			error_detected <= 1'b0;
+		end
+	end
 end
 
+always @ (posedge PERROR or posedge FERROR) begin 
+	error_detected = 1'b1;
+end
 
 // counter
-always@(posedge reset or posedge valid)
+always@(posedge reset or posedge valid or posedge PERROR or posedge FERROR)
 begin
 	if (reset) begin
 		counter = 0;
--- UART/register_tb.v
+++ UART/register_tb.v
@@ -6,48 +6,56 @@ module register_tb;
 reg reset;
 reg [7:0] data_in; 
 reg valid;
+reg PERROR;
+reg FERROR;
 wire[15:0] out;
 
-register register_test(.reset(reset), .data_in(data_in), .valid(valid), .out(out));
+register register_test(.reset(reset), .data_in(data_in), .valid(valid), .PERROR(PERROR), .FERROR(FERROR), .out(out));
 
 initial begin
 	reset = 1; // our reset signal is initialy set to 1
     valid = 1'b0;
+    PERROR = 1'b0;
+    FERROR = 1'b0;  
 
 	#400; // after 100 timing units, i.e. ns
-	reset = 0; // set reset signal to 0
-			
-    data_in = 8'b1001101;
-	
-    # 100;
-    valid = 1'b1;    
+	reset = 0; // set reset signal to 0	
+    data_in = 8'b1001101;   
 
     #10;
     valid = 1'b0;
+    PERROR = 1'b1;
 
-    #10
+    #10;
+    valid = 1'b0;
+    PERROR = 1'b0;
+
+    #10;
     data_in = 8'b11100011;
     
 	#10;
     valid = 1'b1;
+    PERROR = 1'b0; 
+    FERROR = 1'b0;
     
     #10 valid = 1'b0;
+    PERROR = 1'b0; 
+    FERROR = 1'b0;
 
     // second data 
     # 10 data_in = 8'b11111111;
+
     # 10;
     valid = 1'b1;    
 
     #10;
     valid = 1'b0;
 
-    #10
+    #10;
     data_in = 8'b00000000;
     
 	#10;
     valid = 1'b1;
-
-
 end
 
 endmodule
